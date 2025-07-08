1bf5bb8683ce4a65469a9118a31c7761dcb1b0f5
Spandan Barve <spandan567@gmail.com>
2025-07-08 18:23:10 +0530

    test

--- evm/contracts/DatabaseFactory.sol
+++ evm/contracts/DatabaseFactory.sol
@@ -20,10 +20,22 @@ contract DatabaseFactory {
         databasesByOwner[msg.sender] = newDatabase;
         databases.push(address(newDatabase));
 
-        emit DatabaseCreated(msg.sender, "New Database", address(newDatabase));
+        emit DatabaseCreated(msg.sender, address(newDatabase));
     }
 
     function getDatabases() external view returns (address[] memory) {
         return databases;
+    }
+
+    function getDatabaseIndicesByOwner(
+        address owner_
+    ) external view returns (uint256[] memory) {
+        return databasesByOwner[owner_].getAll();
+    }
+
+    function getDatabase(uint256 index_) external view returns (address) {
+        require(index_ < databases.length, "Index out of bounds");
+
+        return databases[index_];
     }
 }
