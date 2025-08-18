beaf3604abe72fbdeb82edea0df3ff02edab56cc
Spandan Barve <spandan567@gmail.com>
2025-08-18 18:00:46 +0530

    Enhance database synchronization in server; add access record tracking for on-chain databases

--- server.ts
+++ server.ts
@@ -1,5 +1,39 @@
+import * as access from "./access";
+import { contracts } from "./evm";
+
 Bun.serve({
   routes: {
+    "/db": {
+      POST: async (req) => {
+        let synced = 0;
+
+        const dbsInStore = access.getDatabaseCount();
+        const dbsOnChain = Number(
+          await contracts.databaseFactory().read.databasesCount()
+        );
+
+        for (let i = dbsInStore; i < dbsOnChain; i++) {
+          const dbAddress = await contracts
+            .databaseFactory()
+            .read.getDatabase([BigInt(i)]);
+
+          const database = await contracts.database(dbAddress);
+
+          const owner = await database.read.owner();
+          const name = await database.read.name();
+
+          access.addDatabaseAccessRecord({
+            owner,
+            name,
+            address: dbAddress,
+          });
+          synced++;
+        }
+
+        return Response.json({ synced });
+      },
+    },
+
     "/raw": {
       POST: async (req) => {
         const sql = await req.text();
